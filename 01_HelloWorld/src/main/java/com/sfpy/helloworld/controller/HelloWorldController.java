f1406e62bf9dc64c62a3a2a9f157dadea737d545
sfpy <sfpy_zk@163.com>
2019-05-09 10:47:55 +0800

    springboot项目搭建

---
+++ 01_HelloWorld/src/main/java/com/sfpy/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.sfpy.helloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/hello1")
+    public String hello1() {
+        return "Hello World";
+    }
+
+    @RequestMapping("/hello2")
+    public List<String> hello2() {
+        return Arrays.asList(new String[] { "A", "B", "C" });
+    }
+}
